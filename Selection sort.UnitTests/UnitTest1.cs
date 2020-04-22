638d6a7005b74375295a35e254c1ffbaf9bcc636
Alexandru Gheorghe <alex.g@tutanota.com>
2020-04-22 21:46:37 +0300

    Finished functional testing

--- Selection sort.UnitTests/UnitTest1.cs
+++ Selection sort.UnitTests/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System.Collections.Immutable;
 using NUnit.Framework;
 
 namespace Selection_sort.UnitTests
@@ -10,18 +9,26 @@ namespace Selection_sort.UnitTests
             Program.Sort(v, asc);
             return v;
         }
-
         [Test]
         public void EquivalencePartitioning()
         {
-            Assert.AreEqual(new int[] { }, Sort(new int[] { }));
-            Assert.AreEqual(new int[] { }, Sort(new int[] { }, false));
-
-            Assert.AreEqual(new [] { 0 }, Sort(new [] { 0 }));
-            Assert.AreEqual(new [] { 0 }, Sort(new [] { 0 }, false));
-
             Assert.AreEqual(new [] { 1, 2, 3 }, Sort(new[] { 3, 1, 2 }));
             Assert.AreEqual(new [] { 3, 2, 1 }, Sort(new[] { 3, 1, 2 }, false));
+        }
+        [Test]
+        public void BoundaryValueAnalysis()
+        {
+            Assert.AreEqual(new int[] { }, Sort(new int[] { }));
+        }
+        [Test]
+        public void CategoryPartitioning()
+        {
+            Assert.AreEqual(new int[] { }, Sort(new int[] { }));
+            Assert.AreEqual(new int[] { }, Sort(new int[] { }, false));
+            Assert.AreEqual(new[] { 0 }, Sort(new[] { 0 }));
+            Assert.AreEqual(new[] { 0 }, Sort(new[] { 0 }, false));
+            Assert.AreEqual(new[] { 1, 2, 3 }, Sort(new[] { 3, 1, 2 }));
+            Assert.AreEqual(new[] { 3, 2, 1 }, Sort(new[] { 3, 1, 2 }, false));
         }
     }
 }
