e414f00db25d471b168e3a2d032a7092ff52b6f6
Alexandru Gheorghe <alex.g@tutanota.com>
2020-04-22 17:15:52 +0300

    Implemented selection sort

--- Selection sort/Program.cs
+++ Selection sort/Program.cs
@@ -10,6 +10,22 @@ namespace Selection_sort
     {
         static void Main(string[] args)
         {
+
+        }
+
+        private void Sort(int[] v, bool asc)
+        {
+            int l = v.Length;
+            for (int i = 0; i < l-1; ++i)
+            {
+                int indexToSwap = i;
+                for (int j = i + 1; j < l; ++j)
+                    if (v[indexToSwap] > v[j] && asc || v[indexToSwap] < v[j] && !asc)
+                        indexToSwap = j;
+                int aux = v[indexToSwap];
+                v[indexToSwap] = v[i];
+                v[i] = aux;
+            }
         }
     }
 }
