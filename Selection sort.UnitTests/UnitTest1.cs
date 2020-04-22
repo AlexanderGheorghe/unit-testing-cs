fe7ab88c4de637e2890c050373e20b0f4034aed6
Alexandru Gheorghe <alex.g@tutanota.com>
2020-04-22 18:11:26 +0300

    added test project

---
+++ Selection sort.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Selection_sort.UnitTests
+{
+    public class SortTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EquivalencePartitioning()
+        {
+            Assert.AreEqual(5,5);
+        }
+    }
+}
